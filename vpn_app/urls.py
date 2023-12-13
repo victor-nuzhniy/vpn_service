da205c873176fe81328ee781a609dfc84d0cca8d
victro-nuzhniy <nuzhniyva@gmail.com>
2023-12-13 20:19:47 +0200

    dev added RegisterView url to urls

--- vpn_app/urls.py
+++ vpn_app/urls.py
@@ -1,8 +1,11 @@
 """Urls for vpn_app."""
 from django.urls import path
 
-from vpn_app.views import IndexView
+from vpn_app.views import IndexView, RegisterView
 
 app_name = "vpn"
 
-urlpatterns = [path("", IndexView.as_view(), name="vpn")]
+urlpatterns = [
+    path("", IndexView.as_view(), name="vpn"),
+    path("sign-up/", RegisterView.as_view(), name="sign_up"),
+]
