8b6af927b39337a99828627c13a54ba164a90b43
victro-nuzhniy <nuzhniyva@gmail.com>
2023-12-13 17:07:34 +0200

    dev created VpnSite model

--- vpn_app/models.py
+++ vpn_app/models.py
@@ -1,3 +1,16 @@
+"""Models for vpn_app."""
 from django.db import models
 
-# Create your models here.
+
+class VpnSite(models.Model):
+    """Model for site data."""
+
+    name = models.CharField(max_length=100, verbose_name="Site name")
+    url = models.URLField(verbose_name="Site url")
+    used_links_number = models.BigIntegerField(verbose_name="Used links number")
+    sended_volume = models.BigIntegerField(verbose_name="Sended data volume")
+    loaded_volume = models.BigIntegerField(verbose_name="Loaded data volume")
+
+    def __str__(self) -> str:
+        """Represent model."""
+        return f"{self.name}"
