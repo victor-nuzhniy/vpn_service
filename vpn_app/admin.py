888419190f81dbd1ac8fe9d2a9bf31bc427817f0
victro-nuzhniy <nuzhniyva@gmail.com>
2023-12-13 17:13:37 +0200

    dev created admin models

--- vpn_app/admin.py
+++ vpn_app/admin.py
@@ -1,3 +1,22 @@
+"""Admin site configuration for vpn_app."""
 from django.contrib import admin
 
-# Register your models here.
+from vpn_app.models import VpnSite
+
+
+class VpnSiteAdmin(admin.ModelAdmin):
+    """VpnSite model admin site configuration."""
+
+    model = VpnSite
+    list_display = (
+        "id",
+        "name",
+        "url",
+        "used_links_number",
+        "sended_volume",
+        "loaded_volume",
+    )
+    list_display_links = ("id", "name", "url")
+
+
+admin.site.register(VpnSite, VpnSiteAdmin)
