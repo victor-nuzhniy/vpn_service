af4ff2984d4e5bc1d50747f531dd7d48648af796
victro-nuzhniy <nuzhniyva@gmail.com>
2023-12-13 20:22:54 +0200

    dev added CustomAuthForm to forms

--- vpn_app/forms.py
+++ vpn_app/forms.py
@@ -1,5 +1,5 @@
 """Forms for vpn_app."""
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -15,5 +15,19 @@ class CustomUserCreationForm(UserCreationForm):
             "class"
         ] = "custom-input custom-input-height"
         self.fields["password2"].widget.attrs[
+            "class"
+        ] = "custom-input custom-input-height"
+
+
+class CustomAuthForm(AuthenticationForm):
+    """Customize AuthenticationForm fields classes."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Restyle fields classes."""
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs[
+            "class"
+        ] = "custom-input custom-input-height"
+        self.fields["password"].widget.attrs[
             "class"
         ] = "custom-input custom-input-height"
