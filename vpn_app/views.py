04b9607d0063b38e7dd744bf95c494af1697b7ac
victro-nuzhniy <nuzhniyva@gmail.com>
2023-12-13 20:36:49 +0200

    dev added CustomPasswordChangeView

--- vpn_app/views.py
+++ vpn_app/views.py
@@ -1,13 +1,17 @@
 """Views for vpn_app."""
 from django.contrib.auth import login
 from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import FormView, TemplateView
 
-from vpn_app.forms import CustomAuthForm, CustomUserCreationForm
+from vpn_app.forms import (
+    CustomAuthForm,
+    CustomPasswordChangeForm,
+    CustomUserCreationForm,
+)
 from vpn_app.mixins import ChangeSuccessURLMixin
 
 
@@ -41,6 +45,15 @@ class CustomLoginView(ChangeSuccessURLMixin, LoginView):
     redirect_authenticated_user = True
     template_name = "vpn_app/auth/login.html"
     form_class = CustomAuthForm
+
+
+class CustomPasswordChangeView(ChangeSuccessURLMixin, PasswordChangeView):
+    """Class view for user password changing."""
+
+    success_url = reverse_lazy("vpn:signup")
+    template_name = "vpn_app/auth/password_change.html"
+    extra_context = {"title": "Password change"}
+    form_class = CustomPasswordChangeForm
 
 
 class VpnView(View):
