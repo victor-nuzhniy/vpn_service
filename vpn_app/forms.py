a4d44a10ce2a1c452e42d9864f0a45b5630e6c86
victro-nuzhniy <nuzhniyva@gmail.com>
2023-12-13 20:17:27 +0200

    dev created vpn_app forms with CustomUserCreationForm

---
+++ vpn_app/forms.py
@@ -0,0 +1,19 @@
+"""Forms for vpn_app."""
+from django.contrib.auth.forms import UserCreationForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """Customize UserCreationForm fields."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Restyle fields classes."""
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs[
+            "class"
+        ] = "custom-input custom-input-height"
+        self.fields["password1"].widget.attrs[
+            "class"
+        ] = "custom-input custom-input-height"
+        self.fields["password2"].widget.attrs[
+            "class"
+        ] = "custom-input custom-input-height"
