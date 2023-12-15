517f4d339fec161164bcd248f548092ad66e742c
victro-nuzhniy <nuzhniyva@gmail.com>
2023-12-15 22:57:34 +0200

    dev added VpnView with only http logic functionality, used backend rewriting of href and src attrs with localhost

--- vpn_app/views.py
+++ vpn_app/views.py
@@ -1,11 +1,20 @@
 """Views for vpn_app."""
-from typing import Dict
+from typing import Dict, Optional
+from urllib.parse import urlsplit, urlunsplit
 
+import bs4
+import requests
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, PasswordChangeView
-from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import F
+from django.http import (
+    HttpResponse,
+    HttpResponseNotFound,
+    HttpResponseRedirect,
+    StreamingHttpResponse,
+)
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import DeleteView, FormView, TemplateView, UpdateView
@@ -19,6 +28,7 @@ from vpn_app.forms import (
 )
 from vpn_app.mixins import ChangeSuccessURLMixin, CustomUserPassesTestMixin
 from vpn_app.models import VpnSite
+from vpn_app.utils import find_sample, find_sample_without_word
 
 
 class IndexView(TemplateView):
@@ -122,9 +132,113 @@ class AccountView(CustomUserPassesTestMixin, ChangeSuccessURLMixin, UpdateView):
         return context
 
 
-class VpnView(View):
+class VpnView(LoginRequiredMixin, View):
     """Vpn view."""
 
     def get(self, request, *args, **kwargs):
         """Handle get request."""
-        return HttpResponse("Hello World!")
+        path: str = self.kwargs.get("path")
+        domain: Optional[str] = None
+        if path_list := path.split("/"):
+            domain = path_list[0]
+        user = request.user
+        if domain and not (
+            vpn_site := VpnSite.objects.filter(
+                owner=user, domain__startswith=domain
+            ).first()
+        ):
+            return HttpResponseNotFound(
+                f"<h2>Error. You haven't registered site with domain {domain}.</h2>"
+            )
+        if (
+            sec_fetch_dest := request.headers.get("Sec-Fetch_Dest")
+        ) and sec_fetch_dest in {
+            "image",
+            "video",
+            "worker",
+            "sharedworker",
+            "serviceworker",
+            "paintworklet",
+            "audio",
+            "audioworklet",
+        }:
+            url = f"http://{request.path[11:]}"
+            response = requests.request(
+                method="GET",
+                url=url,
+                headers=request.headers,
+                timeout=5.0,
+                stream=True,
+            )
+
+            return StreamingHttpResponse(
+                response.raw,
+                # content_type=response.headers.get('content-type'),
+                headers=response.headers,
+                status=response.status_code,
+                reason=response.reason,
+            )
+
+        response = requests.request(
+            method=request.method,
+            url=f"http://{request.path[11:]}",
+            headers=request.headers,
+            cookies=request.COOKIES,
+            files=request.FILES,
+            timeout=20.0,
+        )
+
+        vpn_site.sended_volume = F("sended_volume") + int(
+            response.headers.get("Content-Length", "0")
+        )
+        content_type = response.headers.get("Content-Type")
+        if content_type and content_type.split(";")[0] in {
+            "text/plain",
+            "text/html",
+            "text/csv",
+            "text/xml",
+        }:
+            vpn_site.used_links_number = F("used_links_number") + 1
+        vpn_site.save()
+        host = request.META["HTTP_HOST"]
+        xsoup = bs4.BeautifulSoup(response.text, "html.parser")
+        for elem in xsoup.find_all("a", href=lambda x: find_sample(x, domain)):
+            parsed_url = urlsplit(elem["href"])
+            elem["href"] = urlunsplit(
+                (
+                    parsed_url.scheme,
+                    f"{host}/localhost/{parsed_url.netloc}",
+                    parsed_url.path,
+                    parsed_url.query,
+                    parsed_url.fragment,
+                )
+            )
+        for elem in xsoup.find_all(href=lambda x: find_sample_without_word(x, "http")):
+            elem["href"] = urlunsplit(
+                (
+                    "http",  # TODO check solution concerning scheme
+                    f"{host}/localhost/{domain}",
+                    elem["href"],
+                    "",
+                    "",
+                )
+            )
+        for elem in xsoup.find_all(src=lambda x: find_sample_without_word(x, "http")):
+            elem["src"] = urlunsplit(
+                (
+                    "http",  # TODO check solution concerning scheme
+                    f"{host}/localhost/{domain}",
+                    elem["src"],
+                    "",
+                    "",
+                )
+            )
+
+        final_response = HttpResponse(
+            content=xsoup.prettify(),
+            status=response.status_code,
+            headers=response.headers,
+        )
+        del final_response.headers["Connection"]
+        del final_response.headers["Keep-Alive"]
+        return final_response
