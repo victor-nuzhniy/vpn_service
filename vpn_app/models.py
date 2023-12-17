de7c351e28df512ed888b7f8bf305434b9aeb4a0
victro-nuzhniy <nuzhniyva@gmail.com>
2023-12-17 21:28:42 +0200

    dev_proxy added scheme field to model

--- vpn_app/models.py
+++ vpn_app/models.py
@@ -6,8 +6,13 @@ from django.db import models
 class VpnSite(models.Model):
     """Model for site data."""
 
+    SCHEMES = [("http", "http"), ("https", "https")]
+
     name = models.CharField(max_length=100, verbose_name="Site name")
     domain = models.CharField(max_length=200, verbose_name="Site domain")
+    scheme = models.CharField(
+        max_length=20, choices=SCHEMES, default="http", verbose_name="Scheme"
+    )
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Site owner")
     used_links_number = models.BigIntegerField(
         default=0, verbose_name="Used links number"
