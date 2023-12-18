1af4e6c2b771a661328ea7eb45c0ef806ac470db
victro-nuzhniy <nuzhniyva@gmail.com>
2023-12-18 11:51:51 +0200

    dev_proxy created celery tasks

---
+++ vpn_app/tasks.py
@@ -0,0 +1,34 @@
+"""Celery tasks for vpn_app."""
+from celery import shared_task
+from django.db.models import F
+
+from vpn_app.models import VpnSite
+
+
+@shared_task()
+def add_links_number(user_id: int, domain: str) -> None:
+    """Increase used_links_number model field."""
+    vpn_site = VpnSite.objects.filter(owner_id=user_id, domain=domain).first()
+    if vpn_site:
+        vpn_site.used_links_number = F("used_links_number") + 1
+        vpn_site.save()
+
+
+@shared_task()
+def add_loaded_volume(user_id: int, domain: str, volume: str | int) -> None:
+    """Increase loaded_volume model field."""
+    vpn_site = VpnSite.objects.filter(owner_id=user_id, domain=domain).first()
+    if vpn_site:
+        vpn_site.loaded_volume = F("loaded_volume") + int(volume)
+        vpn_site.save()
+
+
+@shared_task()
+def add_sended_volume(
+    user_id: int, domain: str, volume: str | int, volume_upper: str | int
+) -> None:
+    """Increase loaded_volume model field."""
+    vpn_site = VpnSite.objects.filter(owner_id=user_id, domain=domain).first()
+    if vpn_site:
+        vpn_site.sended_volume = F("sended_volume") + int(volume) + int(volume_upper)
+        vpn_site.save()
