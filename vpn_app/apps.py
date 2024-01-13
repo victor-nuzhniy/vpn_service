08153370bab69cc6b9a626371a6734e563770dc4
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-13 22:26:32 +0200

    formatters format vpn_app apps.py

--- vpn_app/apps.py
+++ vpn_app/apps.py
@@ -1,6 +1,9 @@
+"""Module with configuration vpn_app."""
 from django.apps import AppConfig
 
 
 class VpnAppConfig(AppConfig):
+    """Class with configuration for vpn_app."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "vpn_app"
