a3ed09c56546f35dc5b863d7b1990ffb07b3455d
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-13 17:52:35 +0200

    formatters format vpn_service models.py

--- vpn_app/models.py
+++ vpn_app/models.py
@@ -8,22 +8,28 @@ from vpn_app.validators import validate_domain
 class VpnSite(models.Model):
     """Model for site data."""
 
-    SCHEMES = [("http", "http"), ("https", "https")]
+    schemes = [("http", "http"), ("https", "https")]
 
     name = models.CharField(max_length=100, verbose_name="Site name")
     domain = models.CharField(
-        max_length=200, validators=[validate_domain], verbose_name="Site domain"
+        max_length=200,
+        validators=[validate_domain],
+        verbose_name="Site domain",
     )
     scheme = models.CharField(
-        max_length=20, choices=SCHEMES, default="http", verbose_name="Scheme"
+        max_length=20,
+        choices=schemes,
+        default="http",
+        verbose_name="Scheme",
     )
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Site owner")
     used_links_number = models.BigIntegerField(
-        default=0, verbose_name="Used links number"
+        default=0,
+        verbose_name="Used links number",
     )
     sended_volume = models.BigIntegerField(default=0, verbose_name="Sended data volume")
     loaded_volume = models.BigIntegerField(default=0, verbose_name="Loaded data volume")
 
     def __str__(self) -> str:
         """Represent model."""
-        return f"{self.name}"
+        return str(self.name)
