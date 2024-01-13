7e047eb099cefce6aa3ad29d1a7ff134f56e1049
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-13 12:48:10 +0200

    formatters format config urls

--- vpn_app/validators.py
+++ vpn_app/validators.py
@@ -4,7 +4,7 @@ import re
 from django.core.exceptions import ValidationError
 
 
-def validate_domain(value):
+def validate_domain(string_value: str) -> None:
     """Check whether domain match sample."""
-    if not re.match("[a-zA-Z0-9-:.]+", value):
-        raise ValidationError("Try input a valid domain name.")
+    if not re.match("[a-zA-Z0-9-:.]+", string_value):
+        raise ValidationError({"message": "Try input a valid domain name."})
