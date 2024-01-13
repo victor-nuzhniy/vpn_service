120f58e456fc28e91ab19a8461dc376b48206ddd
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-13 12:50:02 +0200

    formatters format vpn_service init_create_superuser.py

--- vpn_app/management/commands/init_create_superuser.py
+++ vpn_app/management/commands/init_create_superuser.py
@@ -1,26 +1,39 @@
 """Module for creating superuser command on empty db."""
+import logging
 import os
 from typing import Any
 
 from django.contrib.auth.models import User
+from django.core.exceptions import ImproperlyConfigured
 from django.core.management import BaseCommand
 from dotenv import load_dotenv
 
 load_dotenv()
 
+logger: logging.Logger = logging.getLogger()
+
 
 class Command(BaseCommand):
     """Class for initially creating superuser functionality."""
 
-    def handle(self, *args: Any, **options: Any) -> None:
+    def handle(self, *args: Any, **options: Any) -> None:  # noqa WPS110
         """Create initial superuser if it had not been created earlier."""
-        username: str = os.getenv("DJANGO_SUPERUSER_USERNAME")
-        email: str = os.getenv("DJANGO_SUPERUSER_EMAIL")
-        password: str = os.getenv("DJANGO_SUPERUSER_PASSWORD")
-        if not User.objects.count():
-            print(f"Creating account for user {username} email {email}")
+        username: str = os.getenv('DJANGO_SUPERUSER_USERNAME', '')
+        email: str = os.getenv('DJANGO_SUPERUSER_EMAIL', '')
+        password: str = os.getenv('DJANGO_SUPERUSER_PASSWORD', '')
+        if User.objects.count():
+            logger.info('Admin account had already been initialized!')
+        elif all([username, email, password]):
+            logger.info(
+                'Creating account for user {username} email {email}'.format(
+                    username=username,
+                    email=email,
+                ),
+            )
             User.objects.create_superuser(
-                username=username, email=email, password=password
+                username=username,
+                email=email,
+                password=password,
             )
         else:
-            print("Admin account had already been initialized!")
+            raise ImproperlyConfigured('Username, email or passwrod are not set.')
