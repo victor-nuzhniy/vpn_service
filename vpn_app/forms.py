bffe2b63e744b4ccd06929c9fcb49001d0e721ba
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-13 22:33:39 +0200

    formatters refactor vpn_app forms

--- vpn_app/forms.py
+++ vpn_app/forms.py
@@ -2,55 +2,9 @@
 import typing
 
 from django import forms
-from django.contrib.auth.forms import (
-    AuthenticationForm,
-    PasswordChangeForm,
-    UserCreationForm,
-)
 from django.contrib.auth.models import User
 
 from vpn_app.models import VpnSite
-
-
-class CustomUserCreationForm(UserCreationForm):
-    """Customize UserCreationForm fields."""
-
-    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
-        """Restyle fields classes."""
-        fields = self.fields
-        super().__init__(*args, **kwargs)
-        fields["username"].widget.attrs["class"] = "custom-input custom-input-height"
-        fields["password1"].widget.attrs["class"] = "custom-input custom-input-height"
-        fields["password2"].widget.attrs["class"] = "custom-input custom-input-height"
-
-
-class CustomAuthForm(AuthenticationForm):
-    """Customize AuthenticationForm fields classes."""
-
-    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
-        """Restyle fields classes."""
-        super().__init__(*args, **kwargs)
-        fields = self.fields
-        fields["username"].widget.attrs["class"] = "custom-input custom-input-height"
-        fields["password"].widget.attrs["class"] = "custom-input custom-input-height"
-
-
-class CustomPasswordChangeForm(PasswordChangeForm):
-    """Customize PasswordChangeForm fields."""
-
-    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
-        """Restyle fields classes."""
-        super().__init__(*args, **kwargs)
-        fields = self.fields
-        fields["old_password"].widget.attrs[
-            "class"
-        ] = "custom-input custom-input-height"
-        fields["new_password1"].widget.attrs[
-            "class"
-        ] = "custom-input custom-input-height"
-        fields["new_password2"].widget.attrs[
-            "class"
-        ] = "custom-input custom-input-height"
 
 
 class UserAccountForm(forms.ModelForm):
