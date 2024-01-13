b8b4e5b14eb5f42ffd9def1b6ab0ac2d0352d08b
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-13 12:55:41 +0200

    formatters format vpn_service forms.py

--- vpn_app/forms.py
+++ vpn_app/forms.py
@@ -1,4 +1,6 @@
 """Forms for vpn_app."""
+import typing
+
 from django import forms
 from django.contrib.auth.forms import (
     AuthenticationForm,
@@ -13,47 +15,40 @@ from vpn_app.models import VpnSite
 class CustomUserCreationForm(UserCreationForm):
     """Customize UserCreationForm fields."""
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         """Restyle fields classes."""
+        fields = self.fields
         super().__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs[
-            "class"
-        ] = "custom-input custom-input-height"
-        self.fields["password1"].widget.attrs[
-            "class"
-        ] = "custom-input custom-input-height"
-        self.fields["password2"].widget.attrs[
-            "class"
-        ] = "custom-input custom-input-height"
+        fields["username"].widget.attrs["class"] = "custom-input custom-input-height"
+        fields["password1"].widget.attrs["class"] = "custom-input custom-input-height"
+        fields["password2"].widget.attrs["class"] = "custom-input custom-input-height"
 
 
 class CustomAuthForm(AuthenticationForm):
     """Customize AuthenticationForm fields classes."""
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         """Restyle fields classes."""
         super().__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs[
-            "class"
-        ] = "custom-input custom-input-height"
-        self.fields["password"].widget.attrs[
-            "class"
-        ] = "custom-input custom-input-height"
+        fields = self.fields
+        fields["username"].widget.attrs["class"] = "custom-input custom-input-height"
+        fields["password"].widget.attrs["class"] = "custom-input custom-input-height"
 
 
 class CustomPasswordChangeForm(PasswordChangeForm):
     """Customize PasswordChangeForm fields."""
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         """Restyle fields classes."""
         super().__init__(*args, **kwargs)
-        self.fields["old_password"].widget.attrs[
+        fields = self.fields
+        fields["old_password"].widget.attrs[
             "class"
         ] = "custom-input custom-input-height"
-        self.fields["new_password1"].widget.attrs[
+        fields["new_password1"].widget.attrs[
             "class"
         ] = "custom-input custom-input-height"
-        self.fields["new_password2"].widget.attrs[
+        fields["new_password2"].widget.attrs[
             "class"
         ] = "custom-input custom-input-height"
 
@@ -61,38 +56,34 @@ class CustomPasswordChangeForm(PasswordChangeForm):
 class UserAccountForm(forms.ModelForm):
     """Form for User model."""
 
-    class Meta:
+    class Meta:  # noqa WPS306
         """Class Meta for UserAccountForm."""
 
         model = User
         fields = ("username", "email", "first_name", "last_name")
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         """Rewrite fields class."""
         super().__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs[
-            "class"
-        ] = "custom-input custom-input-height"
-        self.fields["email"].widget.attrs["class"] = "custom-input custom-input-height"
-        self.fields["first_name"].widget.attrs[
-            "class"
-        ] = "custom-input custom-input-height"
-        self.fields["last_name"].widget.attrs[
-            "class"
-        ] = "custom-input custom-input-height"
+        fields = self.fields
+        fields["username"].widget.attrs["class"] = "custom-input custom-input-height"
+        fields["email"].widget.attrs["class"] = "custom-input custom-input-height"
+        fields["first_name"].widget.attrs["class"] = "custom-input custom-input-height"
+        fields["last_name"].widget.attrs["class"] = "custom-input custom-input-height"
 
 
 class VpnSiteCreateForm(forms.ModelForm):
     """Form for PersonalSite model instance creation."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         """Rewrite fields styling."""
         super().__init__(*args, **kwargs)
-        self.fields["name"].widget.attrs["class"] = "custom-input custom-input-height"
-        self.fields["domain"].widget.attrs["class"] = "custom-input custom-input-height"
-        self.fields["scheme"].widget.attrs["class"] = "custom-input custom-input-height"
+        fields = self.fields
+        fields["name"].widget.attrs["class"] = "custom-input custom-input-height"
+        fields["domain"].widget.attrs["class"] = "custom-input custom-input-height"
+        fields["scheme"].widget.attrs["class"] = "custom-input custom-input-height"
 
-    class Meta:
+    class Meta:  # noqa WPS306
         """Class Meta for PersonalSiteCreateForm."""
 
         model = VpnSite
